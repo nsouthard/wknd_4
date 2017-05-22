f897ec7efca2d8e4bd8f88c4d1c4e46ba43dc8b2
nsouthard <nathan.southard@gmail.com>
2017-05-22 17:51:55 -0500

    Final upload

--- algo/haystack.rb
+++ algo/haystack.rb
@@ -4,4 +4,4 @@
 
 haystack = {hay: [:hay, :hay, :hay, {hay: {hay: [:hay, {hay: [:hay, :hay, :needle]}, :hay, :hay, :hay]}}, :hay, :hay]}
 
- 
+ p haystack[:hay][3][:hay][:hay][1][:hay][2]
--- algo/missing_letter.rb
+++ algo/missing_letter.rb
@@ -3,7 +3,34 @@
 # bonus: returns a string of all missing letters as a string. ex: find_missing_letter("ace") would return "bd", write your own test.
 
 def find_missing_letter(range)
-  
+  seperated_range = range.chars
+  control = (seperated_range.first..seperated_range.last).to_a
+  missing_letter = ""
+  included_letters = ""
+  control_2 = nil
+  boolean = false
+
+  control.each do |letter|
+    if seperated_range.include?(letter)
+      included_letters << letter
+    elsif included_letters ==range 
+      control_2 = nil
+      boolean = false
+
+    else
+      missing_letter << letter
+      boolean = true
+    end
+  end
+
+  if boolean = true
+    missing_letter
+  elsif boolean = false
+    control_2
+  end
+end
+      
+        
 
 end
 
